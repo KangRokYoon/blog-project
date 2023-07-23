1ec9e643a1fe8100850fb4153da6bbc5d69837ee
KangRok Yoon <ykr0919@naver.com>
2023-07-23 13:15:54 +0900

    style commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,35 @@
 import styled from "styled-components";
-import {BsList} from 'react-icons/bs'
+import { BsList } from 'react-icons/bs'
 
 const HeaderStyled = styled.div`
     padding: 30px;
     background: #011627;
     height: 80px;
     display: flex;
-    /* justify-content:center; */
-
-    .icon {
-        font-size: 60px;
-        color: #eaecef;
-};
+    align-items: center;
 `
+
 const HeaderTitle = styled.p`
     color: #eaecef;
     font-size: 40px;
     font-family: 'Lugrasimo', cursive;
+    text-align: center;
+    margin: 0; /* Remove any default margin to avoid spacing issues */
+    flex: 1; /* Allow the title to take the available space and center it */
 `
 
-
+const IconWrapper = styled.div`
+    font-size: 60px;
+    color: #eaecef;
+`
 
 const Header = () => {
     return (
         <HeaderStyled>
-            <HeaderTitle>KangRok's Blog</HeaderTitle>
-            <BsList className="icon"/>
+            <IconWrapper>
+                <BsList />
+            </IconWrapper>
+            <HeaderTitle className="title">KangRok's Blog</HeaderTitle>
         </HeaderStyled>
     );
 }
